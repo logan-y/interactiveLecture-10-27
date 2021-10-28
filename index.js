f8ee2ff24d4b4d979999553e80bdcc6f70557d15
Logan Young <logany@att.net>
2021-10-28 11:54:26 -0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("test");
+console.log('hello world')
+
+const counterText = document.querySelector('#counter')
+
+const minusBtn = document.querySelector('#minus-btn')
+const resetBtn = document.querySelector('#reset-btn')
+const plusBtn = document.querySelector('#plus-btn')
+
+let count = 0;
+
+function increase() {
+    count++
+    counterText.textContent = count
+    // console.log(count)
+}
+
+function decrease() {
+    count--
+    counterText.textContent = count
+    // console.log(count)
+}
+
+function reset() {
+    count = 0
+    counterText.textContent = count
+    // console.log(count)
+}
+
+
+minusBtn.addEventListener('click', decrease)
+resetBtn.addEventListener('click', reset)
+plusBtn.addEventListener('click', increase)
+
+let themeBtns = document.querySelectorAll(".theme-buttons");
+
+//callback for the forloop below:
+
+let selectTheme = (event) => {
+    const theme = event.target.textContent;
+    document.querySelector("body").className = theme;
+    document.querySelector("main").className = theme;
+
+    let buttons = document.querySelectorAll('button');
+
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].className = theme;
+    }
+} 
+for (let i = 0; i < themeBtns.length; i++) {
+    themeBtns[i].addEventListener('click', selectTheme);
+}
+
